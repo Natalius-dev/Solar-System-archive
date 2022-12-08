4176a7f0503a736dfbc3c32ace653bc5c20ccace
Ant Developers <nataliusgabriel@gmail.com>
2022-12-08 18:33:09 +0700

    added all planets and added controls

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'OrbitControls';
 import { InteractionManager  } from 'three.interactive';
-import { TextureLoader, Vector2 } from 'three';
+import { GUI } from 'dat.gui';
+
+// MODIFYABLE VARIABLES
+
+const speedControl = {speedControl:1};
+const spinControl = {spinControl:0.00000083927};
+const showOrbits = {showOrbits:true};
 
 // ===================== PLANETARY DATA ==================
 
@@ -87,7 +93,7 @@ const mars = {
 
 const jupiter = {
     name: "Jupiter",
-    semi_major: 778.479/8,
+    semi_major: 778.479/12,
     eccentric: 0.0487,
     inclination: 3.13,
     radius: 69173*4,
@@ -95,7 +101,7 @@ const jupiter = {
     satellites: [
         {
         name: "Io",
-        semi_major: 421800*4,
+        semi_major: 0.4217*16,
         eccentric: 0.0041,
         inclination: 2.213,
         radius: 1821.6*8,
@@ -106,11 +112,31 @@ const jupiter = {
 
 const saturn = {
     name: "Saturn",
-    semi_major: 1432.041/8,
+    semi_major: 1432.041/12,
     eccentric: 0.0520,
     inclination: 2.486,
     radius: 57316*4,
     tilt: 26.73,
+    satellites: []
+}
+
+const uranus = {
+    name: "Uranus",
+    semi_major: 2867.043/16,
+    eccentric: 0.0469,
+    inclination: 0.77,
+    radius: 25266*4,
+    tilt: 97.77,
+    satellites: []
+}
+
+const neptune = {
+    name: "Neptune",
+    semi_major: 4514.953/16,
+    eccentric: 0.0097,
+    inclination: 1.77,
+    radius: 24552.5*4,
+    tilt: 28.32,
     satellites: []
 }
 
@@ -145,7 +171,10 @@ function generateOrbit(body, centerX, centerY, count) {
     }
 
     const orbitMesh = new THREE.Line(new THREE.BufferGeometry().setFromPoints( pathArray ), new THREE.LineBasicMaterial({color:"#fff",linewidth:10}) );
-    scene.add(orbitMesh);
+
+    if(showOrbits.showOrbits){
+        scene.add(orbitMesh);
+    }
 
     return [pathArray,orbitMesh];
 }
@@ -170,12 +199,13 @@ function addBody(texture, body) {
 // ======================= 3D LOGIC =======================
 
 const scene = new THREE.Scene();
-const cam = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 150000);
+const cam = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 550000);
 const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementById("graphics")
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 
 const interact = new InteractionManager(renderer, cam, renderer.domElement);
 
@@ -192,7 +222,7 @@ scene.add( axesHelper );
 
 const skyboxMat = new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("textures/stars.jpg")});
 skyboxMat.side = THREE.BackSide;
-scene.add(new THREE.Mesh(new THREE.BoxGeometry(100000,100000,100000), skyboxMat));
+scene.add(new THREE.Mesh(new THREE.BoxGeometry(500000,500000/2,500000), skyboxMat));
 
 // BODY GENERATION
 
@@ -203,93 +233,175 @@ sunTextureMap.magFilter = THREE.NearestFilter;
 sunTextureMap.generateMipmaps = false;
 sunTextureMap.minFilter = THREE.LinearFilter;
 sunObj.material = new THREE.MeshBasicMaterial({map: sunTextureMap});
-const sunLight = new THREE.PointLight("#ffd782",1.5, 25000, 0.35);
+const sunLight = new THREE.PointLight("#ffd782",1.5, 35000, 0.75);
 sunLight.position.copy(sunObj.position);
 scene.add(sunLight);
 
+let orbitSpeed = 134.766507574;
+
 const mercuryObj = addBody("textures/mercury.jpg", mercury);
-const mercuryOrbit = generateOrbit(mercury,sunObj.position.x,sunObj.position.z,134.766507574*mercury.semi_major)[0];
-mercuryObj.position.copy(mercuryOrbit[0]);
+const mercuryOrbit = generateOrbit(mercury,sunObj.position.x,sunObj.position.z,orbitSpeed*mercury.semi_major);
+mercuryObj.position.copy(mercuryOrbit[0][0]);
 let mercuryOrbitIndex = 0;
 
 const venusObj = addBody("textures/venus.jpg", venus);
 venusObj.position.setX(-300);
-const venusOrbit = generateOrbit(venus,sunObj.position.x,sunObj.position.z,134.766507574*venus.semi_major)[0];
-venusObj.position.copy(venusOrbit[0]);
+const venusOrbit = generateOrbit(venus,sunObj.position.x,sunObj.position.z,orbitSpeed*venus.semi_major);
+venusObj.position.copy(venusOrbit[0][0]);
 let venusOrbitIndex = 0;
 
 const earthObj = addBody("textures/earth.png", earth);
-const earthOrbit = generateOrbit(earth,sunObj.position.x,sunObj.position.z,134.766507574*earth.semi_major)[0];
-earthObj.position.copy(earthOrbit[0]);
+const earthOrbit = generateOrbit(earth,sunObj.position.x,sunObj.position.z,orbitSpeed*earth.semi_major);
+earthObj.position.copy(earthOrbit[0][0]);
 let earthOrbitIndex = 0;
 
+let moonOrbitSpeed = 500;
 const moonObj = addBody("textures/moon.jpg", earth.satellites[0]);
-let moonOrbit = generateOrbit(earth.satellites[0],earthObj.position.x,earthObj.position.y,500);
+let moonOrbit = generateOrbit(earth.satellites[0],earthObj.position.x,earthObj.position.z,moonOrbitSpeed);
 moonObj.position.copy(moonOrbit[0][0]);
 let moonOrbitIndex = 0;
 
 const marsObj = addBody("textures/mars.jpg", mars);
-const marsOrbit = generateOrbit(mars,sunObj.position.x,sunObj.position.z,134.766507574*mars.semi_major)[0];
-marsObj.position.copy(marsOrbit[0]);
+const marsOrbit = generateOrbit(mars,sunObj.position.x,sunObj.position.z,orbitSpeed*mars.semi_major);
+marsObj.position.copy(marsOrbit[0][0]);
 let marsOrbitIndex = 0;
 
 const jupiterObj = addBody("textures/jupiter.jpg", jupiter);
-const jupiterOrbit = generateOrbit(jupiter,sunObj.position.x,sunObj.position.z,134.766507574*jupiter.semi_major)[0];
-jupiterObj.position.copy(jupiterOrbit[0]);
+const jupiterOrbit = generateOrbit(jupiter,sunObj.position.x,sunObj.position.z,orbitSpeed*jupiter.semi_major);
+jupiterObj.position.copy(jupiterOrbit[0][0]);
+let jupiterOrbitIndex = 0;
 
+let ioOrbitSpeed = 450;
 const ioObj = addBody("textures/io.jpg", jupiter.satellites[0]);
-const ioOrbit = generateOrbit(jupiter.satellites[0],jupiterObj.position.x,jupiterObj.position.z,10000);
+let ioOrbit = generateOrbit(jupiter.satellites[0],jupiterObj.position.x,jupiterObj.position.z,ioOrbitSpeed);
 ioObj.position.copy(ioOrbit[0][0]);
+let ioOrbitIndex = 0;
 
 const saturnObj = addBody("textures/saturn.jpg", saturn);
-const saturrOrbit = generateOrbit(saturn,sunObj.position.x,sunObj.position.z,134.766507574*saturn.semi_major)[0];
-saturnObj.position.copy(saturrOrbit[0]);
+const saturnRings = new THREE.Mesh( new THREE.RingGeometry((66900*4)/500,(139826*4)/500,100,1,0,degrees_to_radians(360)), new THREE.MeshStandardMaterial({ color:"#fff", side: THREE.DoubleSide, map: new THREE.TextureLoader().load("textures/saturn ring.png"), transparent: true }) );
+saturnRings.setRotationFromAxisAngle(new THREE.Vector3(1,0,0),degrees_to_radians(90));
+saturnRings.rotation.y = degrees_to_radians(saturn.tilt);
+saturnObj.attach(saturnRings);
+const saturnOrbit = generateOrbit(saturn,sunObj.position.x,sunObj.position.z,orbitSpeed*saturn.semi_major);
+saturnObj.position.copy(saturnOrbit[0][0]);
+let saturnOrbitIndex = 0;
+
+const uranusObj = addBody("textures/uranus.jpg", uranus);
+const uranusRings = new THREE.Mesh( new THREE.RingGeometry((26840*4)/500,(51149*4)/500,100,1,0,degrees_to_radians(360)), new THREE.MeshStandardMaterial({ color:"#fff", side: THREE.DoubleSide, map: new THREE.TextureLoader().load("textures/uranus ring.png"), transparent: true }) );
+uranusRings.setRotationFromAxisAngle(new THREE.Vector3(1,0,0),degrees_to_radians(90));
+uranusRings.rotation.y = degrees_to_radians(uranus.tilt);
+uranusObj.attach(uranusRings);
+const uranusOrbit = generateOrbit(uranus,sunObj.position.x,sunObj.position.z,orbitSpeed*uranus.semi_major);
+uranusObj.position.copy(uranusOrbit[0][0]);
+let uranusOrbitIndex = 0;
+
+const neptuneObj = addBody("textures/neptune.jpg", neptune);
+const neptuneOrbit = generateOrbit(neptune,sunObj.position.x,sunObj.position.z,orbitSpeed*neptune.semi_major);
+neptuneObj.position.copy(neptuneOrbit[0][0]);
+let neptuneOrbitIndex = 0;
 
 const light = new THREE.AmbientLight("#fff",0.2);
 scene.add(light);
 
 function animate() {
-    if(mercuryOrbitIndex >= 134.766507574*mercury.semi_major){
+    if(showOrbits.showOrbits === false) {
+        mercuryOrbit[1].visible = false;
+        venusOrbit[1].visible = false;
+        earthOrbit[1].visible = false;
+        marsOrbit[1].visible = false;
+        jupiterOrbit[1].visible = false;
+        saturnOrbit[1].visible = false;
+        uranusOrbit[1].visible = false;
+        neptuneOrbit[1].visible = false;
+    } else {
+        mercuryOrbit[1].visible = true;
+        venusOrbit[1].visible = true;
+        earthOrbit[1].visible = true;
+        marsOrbit[1].visible = true;
+        jupiterOrbit[1].visible = true;
+        saturnOrbit[1].visible = true;
+        uranusOrbit[1].visible = true;
+        neptuneOrbit[1].visible = true;
+    }
+
+    if(mercuryOrbitIndex >= orbitSpeed*mercury.semi_major){
         mercuryOrbitIndex = 0;
     }
-    if(venusOrbitIndex >= 134.766507574*venus.semi_major){
+    if(venusOrbitIndex >= orbitSpeed*venus.semi_major){
         venusOrbitIndex = 0;
     }
-    if(earthOrbitIndex >= 134.766507574*earth.semi_major){
+    if(earthOrbitIndex >= orbitSpeed*earth.semi_major){
         earthOrbitIndex = 0;
     }
-    if(moonOrbitIndex >= 500){
+    if(moonOrbitIndex >= moonOrbitSpeed){
         moonOrbitIndex = 0;
     }
-    if(marsOrbitIndex >= 134.766507574*mars.semi_major){
+    if(marsOrbitIndex >= orbitSpeed*mars.semi_major){
         marsOrbitIndex = 0;
+    }
+    if(jupiterOrbitIndex >= orbitSpeed*jupiter.semi_major){
+        jupiterOrbitIndex = 0;
+    }
+    if(saturnOrbitIndex >= orbitSpeed*saturn.semi_major){
+        saturnOrbitIndex = 0;
+    }
+    if(ioOrbitIndex >= ioOrbitSpeed){
+        ioOrbitIndex = 0;
+    }
+    if(uranusOrbitIndex >= orbitSpeed*uranus.semi_major){
+        uranusOrbitIndex = 0;
+    }
+    if(neptuneOrbitIndex >= orbitSpeed*neptune.semi_major){
+        neptuneOrbitIndex = 0;
     }
 
 	requestAnimationFrame( animate );
     
-    mercuryObj.rotateOnAxis(new THREE.Vector3(0,1,0),0.005);
-    mercuryObj.position.copy(mercuryOrbit[mercuryOrbitIndex]);
+    mercuryObj.rotateOnAxis(new THREE.Vector3(0,1,0),(mercury.radius/8)*spinControl.spinControl);
+    mercuryObj.position.copy(mercuryOrbit[0][mercuryOrbitIndex]);
     
-    venusObj.rotateOnAxis(new THREE.Vector3(0,1,0),0.005);
-    venusObj.position.copy(venusOrbit[venusOrbitIndex]);
+    venusObj.rotateOnAxis(new THREE.Vector3(0,1,0),(venus.radius/8)*spinControl.spinControl);
+    venusObj.position.copy(venusOrbit[0][venusOrbitIndex]);
     
-    earthObj.rotateOnAxis(new THREE.Vector3(0,1,0),0.005);
-    earthObj.position.copy(earthOrbit[earthOrbitIndex]);
+    earthObj.rotateOnAxis(new THREE.Vector3(0,1,0),(earth.radius/8)*spinControl.spinControl);
+    earthObj.position.copy(earthOrbit[0][earthOrbitIndex]);
     
     scene.remove(moonOrbit[1]);
-    moonOrbit = generateOrbit(earth.satellites[0],earthObj.position.x,earthObj.position.z,500);
+    moonOrbit = generateOrbit(earth.satellites[0],earthObj.position.x,earthObj.position.z,moonOrbitSpeed);
     moonObj.lookAt(earthObj.position);
     moonObj.rotateY(225);
     moonObj.position.copy(moonOrbit[0][moonOrbitIndex]);
     
-    marsObj.rotateOnAxis(new THREE.Vector3(0,1,0),0.005);
-    marsObj.position.copy(marsOrbit[marsOrbitIndex]);
+    marsObj.rotateOnAxis(new THREE.Vector3(0,1,0),(mars.radius/8)*spinControl.spinControl);
+    marsObj.position.copy(marsOrbit[0][marsOrbitIndex]);
 
-    mercuryOrbitIndex += 1;
-    venusOrbitIndex += 1;
-    moonOrbitIndex += 1;
-    earthOrbitIndex += 1;
-    marsOrbitIndex += 1;
+    jupiterObj.rotateOnAxis(new THREE.Vector3(0,1,0),(jupiter.radius/12)*spinControl.spinControl);
+    jupiterObj.position.copy(jupiterOrbit[0][jupiterOrbitIndex]);
+
+    scene.remove(ioOrbit[1]);
+    ioOrbit = generateOrbit(jupiter.satellites[0],jupiterObj.position.x,jupiterObj.position.z,ioOrbitSpeed);
+    ioObj.lookAt(jupiterObj.position);
+    ioObj.position.copy(ioOrbit[0][ioOrbitIndex]);
+
+    saturnObj.rotateOnAxis(new THREE.Vector3(0,1,0),(saturn.radius/12)*spinControl.spinControl);
+    saturnObj.position.copy(saturnOrbit[0][saturnOrbitIndex]);
+
+    uranusObj.rotateOnAxis(new THREE.Vector3(0,1,0),(uranus.radius/12)*spinControl.spinControl);
+    uranusObj.position.copy(uranusOrbit[0][uranusOrbitIndex]);
+
+    neptuneObj.rotateOnAxis(new THREE.Vector3(0,1,0),(neptune.radius/12)*spinControl.spinControl);
+    neptuneObj.position.copy(neptuneOrbit[0][neptuneOrbitIndex]);
+
+    mercuryOrbitIndex += speedControl.speedControl;
+    venusOrbitIndex += speedControl.speedControl;
+    moonOrbitIndex += speedControl.speedControl;
+    earthOrbitIndex += speedControl.speedControl;
+    marsOrbitIndex += speedControl.speedControl;
+    jupiterOrbitIndex += speedControl.speedControl;
+    ioOrbitIndex += speedControl.speedControl;
+    saturnOrbitIndex += speedControl.speedControl;
+    uranusOrbitIndex += speedControl.speedControl;
+    neptuneOrbitIndex += speedControl.speedControl;
 
     /*if(cam.position.distanceTo(controlTarget) > 100){
         cam.position.lerp(controlTarget, 0.01);
@@ -301,3 +413,9 @@ function animate() {
 	renderer.render( scene, cam );
 }
 animate();
+
+// GUI
+const gui = new GUI();
+gui.add(speedControl, 'speedControl', 0, 50, 1);
+gui.add(spinControl, 'spinControl', 0, 0.00003, 0.00000000001);
+gui.add(showOrbits, 'showOrbits');
